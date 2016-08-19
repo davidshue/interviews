fa200ca6e2567c7623a8c2beee17da64432a385a
David Shue <david.shue@babycenter.com>
2016-08-19 12:16:08 -0700

    clean ups

--- src/main/java/KeyCount.groovy
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dshue1 on 8/8/16.
- */
-class KeyCount {
-	@Delegate
-	private Map<Integer, Integer> map = new TreeMap<>()
-
-	int total
-}
--- src/test/java/sysdig/SysdigTest.groovy
+++
@@ -1,6 +0,0 @@
-package sysdig
-/**
- * Created by dshue1 on 8/11/16.
- */
-class SysdigTest {
-}
